3fb334c4ae8309d58a8590b6a0b4d0e5641e5ab8
ThomasGumede <73006753+ThomasGumede@users.noreply.github.com>
2021-08-05 07:00:08 +0200

    Update ChefsCard.jsx

--- src/components/SharedComponets/ChefsCard.jsx
+++ src/components/SharedComponets/ChefsCard.jsx
@@ -1,7 +1,7 @@
 const ChefsCard = ({src, fullName}) => {
     return (
         <div className='flex flex-col items-center w-[240px] sm:w-[290px] m-2 bg-[#fffcf7] cursor-pointer rounded-2xl group h-[360px] space-y-4 hover:shadow-xl p-4'>
-            <div data-aos='flip-left' data-aos-duration='3000' data-aos-duration='1000' className="rounded-xl">
+            <div data-aos='flip-left' data-aos-duration='3000' className="rounded-xl">
                 <img src={`${src}`} alt="" className='w-[220px] h-[280px] sm:w-[270px] rounded-2xl group-hover:shadow-xl'/>
             </div>
             <h3 data-aos='fade-down' className="text-2xl text-[#472b20] font-bold font-sans2 group-hover:shadow-xl">
