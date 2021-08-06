e926561919ec9ec68ae27c2e14df37ebe849e472
ThomasGumede <gumedethomas@outlook.com>
2021-08-06 08:06:34 +0200

    master

--- src/components/SharedComponets/NavItems.jsx
+++ src/components/SharedComponets/NavItems.jsx
@@ -1,7 +1,7 @@
 const NavItems = ({href, children}) => {
     return (
-        <li className='mx-5'>
-            <a href={href} className='relative py-9 text-xl text-gray-900 md:text-lg md:py-2 before:hov hover:before:hov2'>
+        <li className='mx-5 my-6 md:my-0'>
+            <a href={href} className='relative text-xl text-gray-900 md:text-lg md:py-2 before:hov hover:before:hov2'>
                 {children}
             </a>
         </li>
